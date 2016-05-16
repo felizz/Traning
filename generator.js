d267fe2eb36fc3978f59fe1a9e2cf0edaecb2a27
Vu Van Tu <tu.vu@felizz.com>
2016-05-16 11:59:47 +0700

    fixed async, promise and genterator

--- generator.js
+++ generator.js
@@ -2,38 +2,38 @@
  * Created by vuvantu on 11/05/2016.
  */
 
+/**
+ * Use a generator to generate 10 files, and print out log to see clearly when
+ * each file writing request is sent and when they are done.
+ *
+ * If possible, mix those generator code with other async task ot see how generator  help the async task to be executed earlier.
+ */
+var fs = require("fs");
 
-
-var file_names = ['file1', 'file2', 'file3'];
+var count = 0;
+function createFile(filename){
+    count += 1;
+    fs.writeFile(filename, count, (error) => {
+        if(error){
+            console.log("cannot write file, " + filename + "error : " + error);
+        }
+        console.log("done " + filename);
+    });
+    return;
+}
+var file_names = ['files/file1.txt', 'files/file2.txt', 'files/file3.txt',
+                'files/file4.txt', 'files/file5.txt', 'files/file6.txt',
+                'files/file7.txt', 'files/file8.txt', 'files/file9.txt',
+                'files/file10.txt'
+                ];
+var promises = [];
 function* fileNameGenerator(){
     for (var i=0; i < file_names.length; i++) {
+        console.log("yield and push out " + file_names[i]);
         yield file_names[i];
     }
 }
-
-/**
- * Use a generator to generate 10 files, and print out log to see clearly when each file writing request is sent and when they are done.
- * If possible, mix those generator code with other async task ot see how generator  help the async task to be executed earlier.
- */
-
 for (var file_name of fileNameGenerator()) {
-    readFile(file_name);
-}
-
-for (var i=0; i < file_names.length; i++) {
-    readFile(file_names[i]);
-}
-
-var file_name_iter = file_names.next();
-while(true){
-    //Use file_name_iter to do something;
-    if(!file_name_iter.done){
-        file_name_iter = file_names.next();//file_name_iter.next()
-    } else {
-
-        break;
-    }
-}
-
-
+    createFile(file_name);
+ }
 
